2dbcf96e53f148c76b51bb69b4e401691158249b
Maud <maud.chevalier15@gmail.com>
2022-12-23 16:32:57 +0100

    Initialisation projet fil rouge Spring

---
+++ src/main/java/com/m2i/calendar/FoProjetFilRougeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoProjetFilRougeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoProjetFilRougeSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m2i/calendar/FoProjetFilRougeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoProjetFilRougeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
